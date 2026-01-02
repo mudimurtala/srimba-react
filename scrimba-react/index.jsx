9988500c66b499f7a82dd00e51e79d9f0f46a8f6
mudimurtala <ibnmuhyideen95@gmail.com>
2026-01-02 12:23:38 +0100

    Add files to the folder

---
+++ scrimba-react/index.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <section>
+        <img src="react-logo.png" />
+        <h1>This is another element</h1>
+    </section>
+)
