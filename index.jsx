d24071443c3bffbb78678285b25d070992bb50ce
mudimurtala <ibnmuhyideen95@gmail.com>
2026-01-03 16:37:57 +0100

    Custom components

--- index.jsx
+++ index.jsx
@@ -2,16 +2,22 @@ import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.getElementById("root"))
 
+function TemporaryName() {
+    return(
+        <main>
+            <img src="react-logo.png" width="40px" alt="React logo"/>
+            <h1>Fun Facts About React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
+
 root.render(
-    <main>
-        <img src="react-logo.png" width="40px" alt="React logo"/>
-        <h1>Fun Facts About React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 200K stars on GitHub</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </main>
+    <TemporaryName/>
 )
