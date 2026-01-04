f64f791ca8424aa861e5dde868b4a085c2006125
mudimurtala <ibnmuhyideen95@gmail.com>
2026-01-04 10:48:49 +0100

    Parent/Child Component

--- index.jsx
+++ index.jsx
@@ -2,37 +2,61 @@ import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.getElementById("root"))
 
-/**
-Challenge: 
-
-Part 2: 
-- Add a `<header>` element with an `<img />` element with the image of the 
-  React logo inside (src="react-logo.png") and make sure to set the 
-  width to something more manageable so it doesn't take up the whole screen.
-  Also, as always, you should include some alt text on the image.
-- Add an `<h1>` with some text describing the page. (E.g. "Reasons
-  I'm excited to learn React"). Place it above the ordered list, then wrap
-  the `<h1>` and `<ol>` inside a `<main>` element to keep our semantic
-  structure flowing well.
-- Add a `<footer>` after the list that says: 
-    "© 20xx <last name here> development. All rights reserved."
+/** Mini Challenge:
+ * 
+ * Move the `header` element from the Page component into 
+ * its own component called "Header"
+ * 
+ * Then render an instance of the Header component inside
+ * the Page component where the `header` used to be.
  */
 
-function Page() {
+/** Challenge: 
+ * Move the `main` element into its own component called "MainContent" 
+ * and render that component inside the Page component.
+ * 
+ * Do the same with the `footer` element, moving it into a new
+ * component called "Footer"
+*/
+
+
+function Header() {
     return(
-        <main>
+        <>
             <header>
                 <img src="react-logo.png" width="40px" alt="React logo"/>
             </header>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-                <li>I am more likely to get a job as a front end developer if I know React</li>
-            </ol>
-            <footer>
+        </>
+    )
+}
+
+function MainContent() {
+    return(
+        <main>
+                <h1>Reasons I'm excited to learn React</h1>
+                <ol>
+                    <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                    <li>I am more likely to get a job as a front end developer if I know React</li>
+                </ol>
+            </main>
+    )
+}
+
+function Footer() {
+    return(
+        <footer>
                 <small>© 2026 mudi development. All rights reserved.</small>
             </footer>
-        </main>
+    )
+}
+
+function Page() {
+    return(
+        <>
+            <Header/>
+            <MainContent/>
+            <Footer/>
+        </>
     )
 }
 
